a50b12348dbc297660ea6fa4c857cf8d9f7a04a6
sufiyan11919 <sufiyanshaikh11919@gmail.com>
2025-05-09 22:14:51 -0700

    test

--- app.py
+++ app.py
@@ -47,8 +47,7 @@ def add_fav():
         db.session.rollback()
         return jsonify(error="already saved"), 409
 
-
-
+# @app.route("/api/fav", methods=["PUT"])
 @app.route("/api/fav", methods=["DELETE"])
 def del_fav():
     email = request.args.get("user", "demo@example.com")
